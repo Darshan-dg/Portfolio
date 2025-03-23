1c7364856f65bd5b933bf838815c2a54c0491548
Darshan Goswami <darshangoswami50@gmail.com>
2025-03-23 15:37:31 +0530

    change name

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
  
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> DARSHAN GOSWAMI </strong>
+                <strong className="main-name"> DARSHAN  </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
