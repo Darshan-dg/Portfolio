b2aada14462e603a963a9399bb71885447acbc92
Darshan Goswami <darshangoswami50@gmail.com>
2025-10-23 17:42:06 +0530

    Add all commint

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Darshan Goswami </span>
+            from <span className="purple"> Rajkot, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as a software developer at Infinity Group Of Company.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed Bsc IT
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
